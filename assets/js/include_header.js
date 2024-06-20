19c487c746e024af2cc2fa08ce9836e66225af65
mjkim <jungle7503@gmail.com>
2024-06-20 17:30:03 +0900

    파일추가

---
+++ assets/js/include_header.js
@@ -0,0 +1,8 @@
+var header_html = 
+'<h1 class="logo">'+
+'<a href="#">Contentree JoongAng Design System</a>'+
+'</h1>'+
+'<div class="rightItem">'+
+'<div class="searchBox"><input type="text" placeholder="검색"></div>'+
+'<button type="button" title="Toggle theme" class="themeBtn">다크모드</button>'+
+'</div>';
