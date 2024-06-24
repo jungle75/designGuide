3c405f889d7fd9af70a61ce17c08542403c317bb
mjkim <jungle7503@gmail.com>
2024-06-24 09:10:53 +0900

    경로수정

--- assets/js/include_lnb.js
+++ assets/js/include_lnb.js
@@ -1,44 +1,44 @@
 var lnb_html = 
 '<!-- menuBox -->'+
 '<div class="menuBox">'+
-'  <div class="title"><a href="../overview/overview.html" class="overview">Overview</a></div>'+
+'  <div class="title"><a href="/designGuide/overview/overview.html" class="overview">Overview</a></div>'+
 '  <ul class="menuList"></ul>'+
 '</div>'+
 '<!--//menuBox end -->'+
 '<!-- menuBox -->'+
 '<div class="menuBox">'+
-'  <div class="title"><a href="../foundation/foundation.html" class="foundation">Foundation</a></div>'+
+'  <div class="title"><a href="/designGuide/foundation/foundation.html" class="foundation">Foundation</a></div>'+
 '  <ul class="menuList">'+
-'    <li><a href="../foundation/color.html">Color</a></li>'+
-'    <li><a href="../foundation/typography.html">Typography</a></li>'+
-'    <li><a href="../foundation/iconography.html">Iconography</a></li>'+
+'    <li><a href="/designGuide/foundation/color.html">Color</a></li>'+
+'    <li><a href="/designGuide/foundation/typography.html">Typography</a></li>'+
+'    <li><a href="/designGuide/foundation/iconography.html">Iconography</a></li>'+
 '  </ul>'+
 '</div>'+
 '<!--//menuBox end -->'+
 '<!-- menuBox -->'+
 '<div class="menuBox">'+
-'  <div class="title"><a href="../component/component.html" class="component">Component</a></div>'+
+'  <div class="title"><a href="/designGuide/component/component.html" class="component">Component</a></div>'+
 '  <ul class="menuList">'+
-'    <li><a href="../component/boxbutton.html">Box button</a></li>'+
-'    <li><a href="../component/textbutton.html">Text button</a></li>'+
-'    <li><a href="../component/input.html">Input</a></li>'+
-'    <li><a href="../component/empty.html">Empty</a></li>'+
-'    <li><a href="../component/table.html"">Table</a></li>'+
-'    <li><a href="../component/popup.html">Popup</a></li>'+
-'    <li><a href="../component/pagination.html">Pagination</a></li>'+
-'    <li><a href="../component/pageIndicator.html">Page indicator</a></li>'+
-'    <li><a href="../component/dropdown.html">Dropdown</a></li>'+
-'    <li><a href="../component/selectionControls.html">Selection controls</a></li>'+
-'    <li><a href="../component/GNB.html">GNB</a></li>'+
-'    <li><a href="../component/SNB.html">SNB</a></li>'+
-'    <li><a href="../component/FNB.html">FNB</a></li>'+
-'    <li><a href="../component/tabs.html">Tabs</a></li>'+
+'    <li><a href="/designGuide/component/boxbutton.html">Box button</a></li>'+
+'    <li><a href="/designGuide/component/textbutton.html">Text button</a></li>'+
+'    <li><a href="/designGuide/component/input.html">Input</a></li>'+
+'    <li><a href="/designGuide/component/empty.html">Empty</a></li>'+
+'    <li><a href="/designGuide/component/table.html"">Table</a></li>'+
+'    <li><a href="/designGuide/component/popup.html">Popup</a></li>'+
+'    <li><a href="/designGuide/component/pagination.html">Pagination</a></li>'+
+'    <li><a href="/designGuide/component/pageIndicator.html">Page indicator</a></li>'+
+'    <li><a href="/designGuide/component/dropdown.html">Dropdown</a></li>'+
+'    <li><a href="/designGuide/component/selectionControls.html">Selection controls</a></li>'+
+'    <li><a href="/designGuide/component/GNB.html">GNB</a></li>'+
+'    <li><a href="/designGuide/component/SNB.html">SNB</a></li>'+
+'    <li><a href="/designGuide/component/FNB.html">FNB</a></li>'+
+'    <li><a href="/designGuide/component/tabs.html">Tabs</a></li>'+
 '  </ul>'+
 '</div>'+
 '<!--//menuBox end -->'+
 '<!-- menuBox -->'+
 '<div class="menuBox">'+
-'  <div class="title"><a href="../casestudies/casestudies.html" class="case">Case studies</a></div>'+
+'  <div class="title"><a href="/designGuide/casestudies/casestudies.html" class="case">Case studies</a></div>'+
 '  <ul class="menuList">'+
 '    <li><a href="#">Login</a></li>'+
 '    <li><a href="#">List</a></li>'+
