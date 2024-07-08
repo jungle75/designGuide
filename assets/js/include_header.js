71bce4ae928d03c42b58476340a95cc636523bd7
jungle75 <jungle7503@gmail.com>
2024-07-08 10:24:06 +0900

    수정

--- assets/js/include_header.js
+++ assets/js/include_header.js
@@ -1,8 +1,8 @@
-var header_html = 
-'<h1 class="logo">'+
-'<a href="/designGuide/index.html">Contentree JoongAng Design System</a>'+
-'</h1>'+
-'<div class="rightItem">'+
-'<div class="searchBox"><input type="text" placeholder="검색 기능은 준비 중입니다!"></div>'+
-'<button type="button" title="Toggle theme" class="themeBtn">다크모드</button>'+
-'</div>';
+var header_html =
+  '<h1 class="logo">' +
+  '<a href="/designGuide/index.html">Contentree JoongAng Design System</a>' +
+  '</h1>' +
+  '<div class="rightItem">' +
+  '<div class="searchBox"><label><input type="text" placeholder="검색 기능은 준비 중입니다!"><label></div>' +
+  '<button type="button" title="Toggle theme" class="themeBtn">다크모드</button>' +
+  '</div>';
